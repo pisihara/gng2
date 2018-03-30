e91d282fc06078bd7c2782421e8a1c90f9acea59
pisihara <paul.isihara@wheaton.edu>
2018-03-30 12:11:39 -0500

    Dr I - Read Tournament Data

--- GNG2.m
+++ GNG2.m
@@ -1,3 +1,7 @@
 %% Main Script for GNG2 DATA ANALYSIS
 clear all; close all; format long;
 [~, ~, surveydata] =xlsread('GNG2DATA.xlsx','A2:AQ3'); %% Read Data Recovered from Sawtooth 
+for i=1:2
+Tournament(i,1)=surveydata(i,10);
+end
+
